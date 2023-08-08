411b6afd008b18d71ff7fbe5d0438b53f253d573
KK <2846016855@qq.com>
2023-08-08 11:21:44 +0800

    useMemo

--- learn_reacthooks/src/08_useMemo的使用/App.jsx
+++ learn_reacthooks/src/08_useMemo的使用/App.jsx
@@ -7,7 +7,7 @@ const HelloWorld = memo(function(props) {
   return <h2>Hello World</h2>
 })
 
-
+// 计算函数本身，可以将这些内容放到外面(公用的)
 function calcNumTotal(num) {
   // console.log("calcNumTotal的计算过程被调用~")
   let total = 0
@@ -22,10 +22,10 @@ const App = memo(() => {
 
   // const result = calcNumTotal(50)
 
-  // 1.不依赖任何的值, 进行计算
+  // 1.不依赖任何的值, 进行计算；优化的其实是返回值，并不是那个函数
   const result = useMemo(() => {
     return calcNumTotal(50)
-  }, [])
+  }, []) // 空数组表示没有依赖任何值，对返回结果作一个依赖
 
   // 2.依赖count
   // const result = useMemo(() => {
@@ -37,7 +37,6 @@ const App = memo(() => {
   // const increment = useCallback(fn, [])
   // const increment2 = useMemo(() => fn, [])
 
-
   // 4.使用useMemo对子组件渲染进行优化
   // const info = { name: "why", age: 18 }
   const info = useMemo(() => ({name: "why", age: 18}), [])
@@ -48,6 +47,7 @@ const App = memo(() => {
       <h2>计数器: {count}</h2>
       <button onClick={e => setCount(count+1)}>+1</button>
 
+      {/* 传入的info没有改变的时候 */}
       <HelloWorld result={result} info={info} />
     </div>
   )
